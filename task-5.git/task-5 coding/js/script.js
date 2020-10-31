3f828410843dbbb937516ca54855c6cb701acf32
ken_shimowada <ken.shimowada2@gmail.com>
2020-10-31 09:52:13 +0900

    201031変更 ・header : pcレイアウト ・スライダー :  pcレイアウト 　├current-line.png含めず 　└画面幅広げたときのmv__slideクラスwidth・height調整要

--- task-5.git/task-5 coding/js/script.js
+++ task-5.git/task-5 coding/js/script.js
@@ -1,1 +1,23 @@
-script.js
+$('.slider').slick({
+    // autoplay:true,
+    // autoplaySpeed:5000,
+    // centerMode: true,
+    // centerPadding: '60px',
+    // slidesToShow: 1,
+    // dots:true,
+
+    // autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: '100px',
+    focusOnSelect: true,
+});
+
+// $('.burger-btn').on('click',function(){//.burger-btnをクリックすると
+//     $('.burger-btn').toggleClass('close');
+//     //.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+//     $('.header__inner__sp').toggleClass('open');
+//     //.gnav__wrapperにopenクラスを付与
+// });
